0927396b3e6068f317fd125c540d1531ac164add
Winter LaMon <winter.lamon@gmail.com>
2017-09-27 04:30:11 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence1)
   sentence1_array = sentence1.split(" ")
-  sentence1_array.each do |word|
+  sentence1_array.collect do |word|
     word.reverse!
   end
   sentence1_array.join(" ")
